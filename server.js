0a4f015c7f546b5976024849ffeb17c8a5cf7f15
Angelo González <renatoangelo4@gmail.com>
2021-10-24 14:32:44 -0400

    Models finished

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const mongoose = require("mongoose");
+// eslint-disable-next-line no-undef
 mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.connection.on('error', (err) => {
@@ -13,20 +14,33 @@ mongoose.connection.once('open', () => {
 
 // Bring in the models
 
-// require('./models/User');
-// require('./models/Chatroom');
-// require('./models/Message');
-
+require('./models/Affiliate');
+require('./models/AffiliateType');
+require('./models/ARS');
+require('./models/Credentials');
+require('./models/Dependent');
+require('./models/DocType');
+require('./models/Document');
+require('./models/MedicalProfessional');
+require('./models/Profile');
+require('./models/PSS');
 
 const app = require('./app');
 
+// eslint-disable-next-line no-undef
 const server = app.listen(process.env.PORT || 8000, () =>{
     console.log("Server listening on port 8000")
 });
 
 const jwt = require('jwt-then');
 
-const Message = mongoose.model("Message"); 
-const User = mongoose.model("User"); 
-
-
+const Affiliate = mongoose.model("Affiliate"); 
+const AffiliateType = mongoose.model("AffiliateType"); 
+const ARS = mongoose.model("ARS"); 
+const Credentials = mongoose.model("Credentials"); 
+const Dependent = mongoose.model("Dependent"); 
+const DocType = mongoose.model("DocType"); 
+const Document = mongoose.model("Document"); 
+const MedicalProfessional = mongoose.model("MedicalProfessional"); 
+const Profile = mongoose.model("Profile"); 
+const PSS = mongoose.model("PSS"); 
